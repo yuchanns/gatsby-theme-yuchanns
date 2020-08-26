1ae27ca3272340ce0b7ba6a3c7d5e3007444d1c3
yuchanns <airamusume@gmail.com>
2020-08-26 17:59:05 +0800

    add smooth scroll behavior on page transition

--- yuchanns/gatsby-browser.js
+++ yuchanns/gatsby-browser.js
@@ -1,1 +1,17 @@
 import './styles/global.scss'
+import scrollTo from 'gatsby-plugin-smoothscroll'
+
+export const shouldUpdateScroll = ({
+    routerProps: { location },
+}) => {
+    const transitionDelay = 500
+    if (location.action === 'PUSH') {
+        window.setTimeout(() => scrollTo('html'), transitionDelay)
+    } else {
+        window.setTimeout(
+            () => scrollTo('html'),
+            transitionDelay
+        )
+    }
+    return false
+}
