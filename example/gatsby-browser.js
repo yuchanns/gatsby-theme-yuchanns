aade6fd038869ae251ac9c211f0bda5b4983ad86
yuchanns <airamusume@gmail.com>
2020-12-27 03:00:56 +0800

    add push util

--- example/gatsby-browser.js
+++ example/gatsby-browser.js
@@ -1,23 +1,7 @@
+import { fetchSinaNews } from './utils/use-sina'
+import { baiduPush } from './utils/use-baidu'
+
 export const onRouteUpdate = ({ location }) => {
-  if (location.pathname === '/r/news/sina' || location.pathname === '/') {
-    fetch('https://ruanyf.github.io/sina-news/rss.json')
-      .then(async function (response) {
-        const res = await response.json();
-        const items = res.items;
-
-        const list = document.querySelector('.list');
-        const fragment = document.createDocumentFragment();
-
-        items.forEach(i => {
-          const li = document.createElement('li');
-          const p = document.createElement('p');
-          const timeObj = new Date(i.date_modified);
-          p.innerHTML = `${i.title} （<a href="${i.url}" target="_blank">${timeObj.getHours()}:${timeObj.getMinutes()}</a>）`;
-          li.appendChild(p);
-          fragment.appendChild(li);
-        });
-
-        list.appendChild(fragment);
-      })
-  }
+  fetchSinaNews(location)
+  baiduPush()
 }
