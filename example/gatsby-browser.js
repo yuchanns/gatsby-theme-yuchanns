7f0739fdc5fe6220f3284bff27bed8904901ce00
yuchanns <airamusume@gmail.com>
2021-01-12 23:54:26 +0800

    remove push script

--- example/gatsby-browser.js
+++ example/gatsby-browser.js
@@ -1,7 +1,5 @@
 import { fetchSinaNews } from './utils/use-sina'
-import { baiduPush } from './utils/use-baidu'
 
 export const onRouteUpdate = ({ location }) => {
   fetchSinaNews(location)
-  baiduPush()
 }
